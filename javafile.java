3d92952c62c1dc54d2ebc113235ae941453e2eec
srikar1506 <bssuryasrikar2002@gmail.com>
2024-04-03 16:57:22 +0530

    it is a java file

---
+++ javafile.java
@@ -0,0 +1,15 @@
+class javafile
+{
+	public static void main(String[] args)
+	{
+		int n=94;
+		if(n%2==0)
+		{
+			System.out.println("it is even");
+		}
+		else
+		{
+			System.out.println("it is odd");
+		}
+	}
+}
